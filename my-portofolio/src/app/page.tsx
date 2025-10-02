1325b86f62c1cac75ed6545cf743e1e64b438563
yesia plorina <yesiaplorina@gmail.com>
2025-10-02 23:42:22 +0700

    Fix image source paths by removing leading slashes

--- my-portofolio/src/app/page.tsx
+++ my-portofolio/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="file.svg"
             alt="File icon"
             width={16}
             height={16}
