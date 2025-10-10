6fac039a7098aaed5f1905954ba1c88f3846bc27
yesia plorina <yesiaplorina@gmail.com>
2025-10-10 14:54:16 +0700

    Add Babel configuration for Jest testing with support for ES6, React, and TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest"
+    "^.+\\.(ts|tsx|js|jsx)$": ["babel-jest", { configFile: "./babel-jest.config.js" }]
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
