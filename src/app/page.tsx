259844a78a5e9c68b5ffb3ed9e3eaeb04645d702
yesia plorina <yesiaplorina@gmail.com>
2025-10-15 16:17:43 +0700

    Add metadata export for Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import HomePage from "./page/HomePage/HomePage";
 
+export const metadata = {
+  title: "Portofolio Yesia plorina",
+};
 export default function Home() {
   return (
     <HomePage />
