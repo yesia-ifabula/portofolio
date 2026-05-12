b2c5c6681de1acfcbe7ccccf309345f48d1a5e2b
yesiaplorina <yesia.plorina@ifabula.com>
2026-05-12 19:00:42 +0700

    Updates

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": ["babel-jest", { configFile: "./babel-jest.config.js" }]
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// ...existing code...
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
-// ...existing code...
